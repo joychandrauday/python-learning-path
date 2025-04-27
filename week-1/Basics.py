d821c1429af5db043615c68c6462853711abeadf
Joy Chandra Uday <joychandraud@gmail.com>
2025-04-28 01:37:34 +0600

    exploring basic variables and  concating methods

---
+++ week-1/Basics.py
@@ -0,0 +1,13 @@
+# variables and constants
+patient_name= "Joy Chandra"
+age=20
+is_admitted=True
+# CONCATING
+# concating using str()
+print("concating using str() => the patient name is:"+patient_name+". His age is "+str(age)+".")
+
+# concating using f-string
+print(f"concating using f-string => the patient name is:{patient_name}. His age is {age}.")
+
+# concating using format()
+print("concating using format() => the patient name is:{}. His age is {}.".format(patient_name,age))
