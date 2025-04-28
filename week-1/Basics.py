051a7737755e50902e0683d0e27284627f6fb333
Joy Chandra Uday <joychandraud@gmail.com>
2025-04-28 16:05:30 +0600

    Set Up Week-1 folder

--- week-1/Basics.py
+++ week-1/Basics.py
@@ -2,6 +2,7 @@
 patient_name= "Joy Chandra"
 age=20
 is_admitted=True
+
 # CONCATING
 # concating using str()
 print("concating using str() => the patient name is:"+patient_name+". His age is "+str(age)+".")
@@ -11,3 +12,11 @@ print(f"concating using f-string => the patient name is:{patient_name}. His age 
 
 # concating using format()
 print("concating using format() => the patient name is:{}. His age is {}.".format(patient_name,age))
+
+'''
+multi
+line
+comment
+'''
+
+# If else
