cc2a3ded0f42be35ecb56c57be1c3278465370c9
Joy Chandra Uday <joychandraud@gmail.com>
2025-04-29 00:42:52 +0600

    exploring for while loop

--- control-flow.py
+++ control-flow.py
@@ -3,9 +3,42 @@ x=5
 y=5
 
 # if else and else if
-if x>y:
-    print("x is greater than y.")
-elif x==y:
-    print("X is equal to Y.")
-else:
-    print("X is not greater than Y")
+# if x>y:
+#     print("x is greater than y.")
+# elif x==y:
+#     print("X is equal to Y.")
+# else:
+#     print("X is not greater than Y")
+
+
+# for loop
+# c=4
+# for i in range(c):
+#     print(i)
+
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+
+# for loop practice task #
+
+# printing each char of a string 
+# string="python"
+# for single_char in string:
+#     print(single_char)
+
+# printing each number after multiplying 2
+number_array=[1,2,3,4,5,6]
+sum=0
+for number in number_array:
+    sum=sum+number
+    print(sum)
+
+
+
+# while loop
+# m=5
+# count=0
+# while count<m:
+#     print(count)
+#     count+=1
