8d2ff9a9cc7540c893e89460da11a0e7867a3a5f
Joy Chandra Uday <joychandraud@gmail.com>
2025-05-02 23:28:12 +0600

    Update dataStructureTUPLE.py

--- dataStructureTUPLE.py
+++ dataStructureTUPLE.py
@@ -2,3 +2,9 @@ first_Tuple = (10, 20, 30, "banana", True)
 # unpacking  tuple 
 a, b, c, d, e = first_Tuple
 print(a)  # 10
+
+print(my_tuple[2])   # 30first_Tuple = (10, 20, 30, "banana", True)
+
+# unpacking  tuple 
+a, b, c, d, e = first_Tuple
+print(a)  # 10
