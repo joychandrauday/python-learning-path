a9a9147b0ce918ad22c62adb0b3534cb8799d928
Joy Chandra Uday <joychandraud@gmail.com>
2025-05-13 22:55:18 +0600

    File accessing

--- fileAccessing.py
+++ fileAccessing.py
@@ -1,2 +1,17 @@
-text=open('test.txt')
-print(text.readlines(4))
+# text=open('test.txt')
+# print(text.readlines(4))
+# with open("test.txt", "w") as file:
+#     file.write("Hello, File!")
+# print(text.read())
+
+# 1.open(filename, mode)
+file = open("example.txt", "w")  # Opens file for writing
+# file.write("Hello, World!")
+# file.close()
+
+# 2.read
+file = open("test.txt", "r")
+content = file.read()
+print(content)
+file.close()
+
